11372816ef8203ca3dcba040012e741ae70336f2
huy <dinhhuyams@gmail.com>
2020-09-30 07:40:55 +0300

    feat: add fragment

--- app/src/main/java/com/example/runningpro/ui/MainActivity.kt
+++ app/src/main/java/com/example/runningpro/ui/MainActivity.kt
@@ -1,8 +1,9 @@
-package com.example.runningpro
+package com.example.runningpro.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.example.runningpro.R
 import com.example.runningpro.db.RunDao
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
---
+++ app/src/main/java/com/example/runningpro/ui/fragments/SetupFragment.kt
@@ -0,0 +1,6 @@
+package com.example.runningpro.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.runningpro.R
+
+class SetupFragment: Fragment(R.layout.fragment_setup)
